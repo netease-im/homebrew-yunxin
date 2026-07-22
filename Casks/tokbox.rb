7f49dd0af1d6667ee05578a1836f327dd460e049
Dylan <2894220@gmail.com>
2026-07-22 17:47:11 +0800

    tokbox: update to 0.7.0

--- Casks/tokbox.rb
+++ Casks/tokbox.rb
@@ -1,8 +1,8 @@
 cask "tokbox" do
-  version "0.6.2"
-  sha256 "ea15afe767a0337b1e5a4a79a7c244b641c7ba936cb455f07ad8b17391ba1b2f"
+  version "0.7.0"
+  sha256 "70b6d2190ee1dd25c9332a705ac6c4daefe08d2d4c1467ca4308f34a9089b3e8"
 
-  url "https://yx-web-nosdn.netease.im/package/1784625243233/Tokbox_#{version}_universal.dmg"
+  url "https://yx-web-nosdn.netease.im/package/1784713438909/Tokbox_#{version}_universal.dmg"
   name "Tokbox"
   desc "NetEase IM desktop client"
   homepage "https://tokbox.netease.im/"
